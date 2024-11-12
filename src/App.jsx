23d5da133a03f196eaa3617c159d4a15f1fd9b9d
Philip-Machar <philipmachar788@gmail.com>
2024-11-12 17:26:41 +0300

    Added dynamic die change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import Die from "./components/Die"
 
 const App = () => {
+  const allNewDice = () => {
+    const newDice = [];
+
+    for(let i=0; i < 10; i++){
+      const randomNum = Math.ceil(Math.random() * 6);
+      newDice.push(randomNum)
+    }
+
+    return newDice;
+  };
+
+  const [die, setDie] = useState(allNewDice());
+
   return (
     <main className="w-[500px] h-[520px] bg-[#0B2434] grid place-content-center shadow-xl">
       <div className="w-[460px] h-[460px] bg-[#F5F5F5] rounded-xl flex justify-center items-center">
         <div className="grid grid-cols-5 gap-6">
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          <Die value={6} />
-          <Die value={7} />
-          <Die value={8} />
-          <Die value={9} />
-          <Die value={10} />
+          {
+            die.map((dice, index) => {
+              return (
+                <Die key={index} value={dice}/>
+              )
+            })
+          }
         </div>
       </div>
     </main>
