0038a0e652546b95da47223901ad667171f52bcf
Philip-Machar <philipmachar788@gmail.com>
2024-11-12 16:39:31 +0300

    Added die

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react"
+import Die from "./components/Die"
 
 const App = () => {
   return (
-    <main className="w-[500px] h-[520px] bg-[#0B2434] grid place-content-center">
-      <div className="w-[460px] h-[460px] bg-[#F5F5F5] rounded-xl">
-
+    <main className="w-[500px] h-[520px] bg-[#0B2434] grid place-content-center shadow-xl">
+      <div className="w-[460px] h-[460px] bg-[#F5F5F5] rounded-xl flex justify-center items-center">
+        <div className="grid grid-cols-5 gap-6">
+          <Die value={1} />
+          <Die value={2} />
+          <Die value={3} />
+          <Die value={4} />
+          <Die value={5} />
+          <Die value={6} />
+          <Die value={7} />
+          <Die value={8} />
+          <Die value={9} />
+          <Die value={10} />
+        </div>
       </div>
     </main>
   )
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+const Die = (props) => {
+  return (
+    <div className="w-[55px] h-[55px] bg-white grid place-content-center rounded-lg shadow-lg cursor-pointer">
+      <div className="text-[#2B283A] font-bold text-2xl">{props.value}</div>
+    </div>
+  )
+}
+
+export default Die
